943a8f4dec3a6e4e4510c37d0c337f137253e9b6
dkswami <dkswami1820@gmail.com>
2022-09-02 15:42:26 +0530

    showing a single article with dynamic routing

--- app/controllers/myarticles_controller.rb
+++ app/controllers/myarticles_controller.rb
@@ -2,4 +2,7 @@ class MyarticlesController < ApplicationController
   def index
 	@articles = Myarticle.all
   end
+  def show
+	@article = Myarticle.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
 	root "myarticles#index"
 
 	get "/myarticles", to: "myarticles#index"
+	get "/myarticles/:id", to:"myarticles#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
