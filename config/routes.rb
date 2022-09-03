559e26245eb7945e1f482632b0f3fd43cd5a874b
dkswami <dkswami1820@gmail.com>
2022-09-03 23:40:58 +0530

    added resourceful routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
 	root "myarticles#index"
 
-	get "/myarticles", to: "myarticles#index"
-	get "/myarticles/:id", to:"myarticles#show"
+	resources :myarticles
+	#get "/myarticles", to: "myarticles#index"
+	#get "/myarticles/:id", to:"myarticles#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
