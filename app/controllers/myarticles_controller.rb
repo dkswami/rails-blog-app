b9d32229a7d395307e5d2769f4fb9924b86b802d
dkswami <dkswami1820@gmail.com>
2022-09-05 14:39:07 +0530

    7.5 added delete feature hence crud completed

--- app/controllers/myarticles_controller.rb
+++ app/controllers/myarticles_controller.rb
@@ -35,6 +35,13 @@ class MyarticlesController < ApplicationController
 	end
   end
 
+  def destroy
+	@article = Article.find(params[:id])
+	@article.destroy
+   
+	redirect_to root_path, status: :see_other
+     end
+
   private
   	def myarticle_params
 		params.require(:myarticle).permit(:title, :body)
